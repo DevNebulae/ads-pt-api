ebd65f5e1882bbd2a2290da18e2dd68f59c72a28
DevNebulae <gamerkeeperson+github@gmail.com>
2017-10-06 15:10:44 +0200

    Update resolvers with cleaner syntax

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,14 +1,16 @@
 export default {
 	Query: {
-		items: (root, args, context) => context.database.collection('rsbuddy').find({}, {transactions: false}).toArray(),
-		item: async (root, {id}, context) => {
-			return (await context.database.collection('rsbuddy').find({id}, {transactions: false}).toArray())[0]
-		}
+		items: async (root, {ids}, context) => {
+			if (ids)
+				return context.database.collection('rsbuddy').find({id: {$in: ids}}).toArray()
+			else
+				return context.database.collection('rsbuddy').find({}, {transactions: false}).toArray()
+		},
+		item: (root, {id}, context) => context.database.collection('rsbuddy').findOne({id}, {transactions: false})
 	},
 	Item: {
-		transactions: async (parent, args, context) => {
-			return (await context.database.collection('rsbuddy').find({id: parent.id}, {_id: false, transactions: true}).next()).transactions
+		transactions: async (root, args, context) => {
+			return (await context.database.collection('rsbuddy').findOne({id: root.id}, {_id: false, transactions: true})).transactions
 		}
 	}
 }
-
