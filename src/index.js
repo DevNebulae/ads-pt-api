5a11dadccc0ef8399824a9e7b4307b947909bdaf
DevNebulae <gamerkeeperson+github@gmail.com>
2017-10-06 15:09:49 +0200

    Add index.js file and split old contents to server.js file

--- src/index.js
+++ src/index.js
@@ -1,32 +1,19 @@
-import express from 'express';
-import cors from 'cors';
-import {
-  graphqlExpress,
-  graphiqlExpress,
-} from 'graphql-server-express';
-import bodyParser from 'body-parser';
-import {MongoClient} from "mongodb"
+import {execute, subscribe} from "graphql";
+import http, {createServer} from "http";
+import {app, PORT} from "./server";
+import schema from "./schema";
 
-import schema from './schema';
+const server = http.createServer(app);
+let currentApp = app;
 
-const MONGO_URL = "mongodb://localhost:27017/rsbuddy"
-const PORT = 7700;
-const app = express();
-
-app.use('*', cors({ origin: 'http://localhost:8000' })); //Restrict the client-origin for security reasons.
-
-app.use('/graphql', bodyParser.json(), graphqlExpress(async () => ({
-	schema,
-	context: {
-		database: await MongoClient.connect(MONGO_URL)
-	}
-})))
-
-app.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql'
-}));
-
-app.listen(PORT, () => {
-	console.log(`GraphQL Server is now running on http://localhost:${PORT}`)
+server.listen(PORT, () => {
+	console.log(`GraphQL-server listening on port ${PORT}.`)
 });
 
+if (module.hot) {
+	module.hot.accept(["./server", "./schema"], () => {
+		server.removeListener("request", currentApp);
+		server.on("request", app);
+		currentApp = app;
+	});
+}
