9b00e5ee4aee2ac7ce60d0382f497dc12edd9460
DevNebulae <gamerkeeperson+github@gmail.com>
2017-10-06 15:11:07 +0200

    Add RSBuddy mongoose schema

---
+++ src/schemas/rsbuddy.js
@@ -0,0 +1,25 @@
+import {Schema} from "mongoose"
+
+export const RSBuddy = new Schema({
+	ts: {
+		type: String
+	},
+	buyingPrice: {
+		type: Number
+	},
+	buyingCompleted: {
+		type: Number
+	},
+	sellingPrice: {
+		type: Number
+	},
+	sellingCompleted: {
+		type: Number
+	},
+	overallPrice: {
+		type: Number
+	},
+	overallCompleted: {
+		type: Number
+	}
+})
