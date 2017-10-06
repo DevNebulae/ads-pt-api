ee7ebc68526acbc7a5757c92f5cce1f405ac4ff4
DevNebulae <gamerkeeperson+github@gmail.com>
2017-10-06 15:10:29 +0200

    Split GraphQL types into a seperate file

--- src/schema.js
+++ src/schema.js
@@ -1,28 +1,6 @@
 import { makeExecutableSchema } from "graphql-tools"
 import resolvers from "./resolvers"
 
-const typeDefs = `
-	type Transaction {
-		ts: String,
-		buyingPrice: Int,
-		buyingCompleted: Int,
-		sellingPrice: Int,
-		sellingCompleted: Int,
-		overallPrice: Int,
-		overallCompleted: Int
-	}
-
-	type Item {
-		id: Int,
-		name: String,
-		store: String,
-		transactions: [Transaction]
-	}
-
-	type Query {
-		items: [Item]
-		item(id: Int!): Item
-	}
-`
+const typeDefs = require('./graphql/schema.gql')
 
 export default makeExecutableSchema({typeDefs, resolvers})
