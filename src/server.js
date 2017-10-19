3bfcbc3ce266e873e43b8faf15d7b9ca87e6a094
DevNebulae <gamerkeeperson+github@gmail.com>
2017-10-19 20:16:30 +0200

    Add mongoose debugging and move models to context

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
+import bodyParser from "body-parser"
 import express from "express"
 import graphqlHTTP from "express-graphql"
 import { graphqlExpress, graphiqlExpress } from "graphql-server-express"
-import bodyParser from "body-parser"
-import mongoose, { Schema } from "mongoose"
+import mongoose from "mongoose"
 import schema from "./graphql"
 import Item from "./db/item"
+import Update from "./db/update"
 
 // Constants
 const CONFIG = require("./settings.yaml")
@@ -13,20 +14,31 @@ const app = express()
 
 mongoose.Promise = global.Promise
 mongoose.connect(
-  `mongodb://${CONFIG.mongodb.url}:${CONFIG.mongodb.port}/${CONFIG.mongodb.database}`,
+  `mongodb://${CONFIG.mongodb.url}:${CONFIG.mongodb.port}/${CONFIG.mongodb
+    .database}`,
   {
     useMongoClient: true
   }
 )
+mongoose.set("debug", true)
 
 app.use(
   "/graphql",
   bodyParser.json({
     limit: "15mb"
   }),
-  graphqlExpress(() => ({
-    schema
-  }))
+  graphqlExpress(req => {
+    return {
+      schema,
+      context: {
+        loaders: {},
+        models: {
+          items: Item,
+          updates: Update
+        }
+      }
+    }
+  })
 )
 
 app.use(
