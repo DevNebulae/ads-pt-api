5de295e76b4c67c1f66f451a68435e2bfd3c5cb6
DevNebulae <gamerkeeperson+github@gmail.com>
2017-10-26 17:30:25 +0200

    Update index.js file with async start function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
+import colors from "colors"
 import http from "http"
+import models from "./db"
 import { app, CONFIG } from "./server"
 
-let currentApp = app
-const server = http.createServer(app)
-server.listen(CONFIG.port)
-console.log(`GraphQL-server listening on port ${CONFIG.port}.`)
+async function start() {
+  // Variables for hot reloading
+  let currentApp = app
 
-if (module.hot) {
-  module.hot.accept(["./server", "./graphql"], () => {
-    server.removeListener("request", currentApp)
-    server.on("request", app)
-    currentApp = app
-  })
+  await models.sequelize.authenticate()
+  await models.sequelize.sync()
+  console.info("Database has been initialized".cyan)
+
+  const server = http.createServer(app)
+
+  server.listen(CONFIG.port)
+  console.info(`GraphQL-server has been started on port ${CONFIG.port}`.yellow)
+
+  if (module.hot)
+    module.hot.accept(["./server"], () => {
+      server.removeListener("request", currentApp)
+      server.on("request", app)
+      currentApp = app
+    })
 }
+
+start()
