dc569d962c0d6b97ce4dd882af28ea2b33920734
DevNebulae <gamerkeeperson+github@gmail.com>
2017-10-26 23:13:21 +0200

    Add freezeTableName property to rsbuddy schema

--- src/models/rsbuddy.js
+++ src/models/rsbuddy.js
@@ -1,39 +1,43 @@
 export default (sequelize, DataTypes) => {
-  const rsbuddy = sequelize.define("rsbuddy", {
-    id: {
-      defaultValue: DataTypes.UUIDV1,
-      primaryKey: true,
-      type: DataTypes.UUID
+  const rsbuddy = sequelize.define(
+    "rsbuddy",
+    {
+      id: {
+        defaultValue: DataTypes.UUIDV1,
+        primaryKey: true,
+        type: DataTypes.UUID
+      },
+      ts: {
+        allowNull: false,
+        type: DataTypes.BIGINT
+      },
+      buyingPrice: {
+        field: "buying_price",
+        type: DataTypes.BIGINT
+      },
+      buyingCompleted: {
+        field: "buying_completed",
+        type: DataTypes.BIGINT
+      },
+      sellingPrice: {
+        field: "selling_pice",
+        type: DataTypes.BIGINT
+      },
+      sellingCompleted: {
+        field: "selling_completed",
+        type: DataTypes.BIGINT
+      },
+      overallPrice: {
+        field: "overall_price",
+        type: DataTypes.BIGINT
+      },
+      overallCompleted: {
+        field: "overall_completed",
+        type: DataTypes.BIGINT
+      }
     },
-    ts: {
-      allowNull: false,
-      type: DataTypes.BIGINT
-    },
-    buyingPrice: {
-      field: "buying_price",
-      type: DataTypes.BIGINT
-    },
-    buyingCompleted: {
-      field: "buying_completed",
-      type: DataTypes.BIGINT
-    },
-    sellingPrice: {
-      field: "selling_pice",
-      type: DataTypes.BIGINT
-    },
-    sellingCompleted: {
-      field: "selling_completed",
-      type: DataTypes.BIGINT
-    },
-    overallPrice: {
-      field: "overall_price",
-      type: DataTypes.BIGINT
-    },
-    overallCompleted: {
-      field: "overall_completed",
-      type: DataTypes.BIGINT
-    }
-  })
+    { freezeTableName: true }
+  )
 
   return rsbuddy
 }
