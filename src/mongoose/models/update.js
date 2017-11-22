852b386eddfd29b0344d68c0ef4362ffa1ed24d1
DevNebulae <gamerkeeperson+github@gmail.com>
2017-11-22 22:56:45 +0100

    Fix error of function as a constructor

--- src/mongoose/models/update.js
+++ src/mongoose/models/update.js
@@ -1,6 +1,6 @@
-import mongoose, { Schema } from "mongoose"
+import mongoose from "mongoose"
 
-const update = new Schema({
+const update = mongoose.Schema({
   title: {
     type: String,
     required: true
